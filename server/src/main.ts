1000e54afd0b3c41f99cd171b151269eec9a46cc
Rufus251 <vladimerrybalkin@gmail.com>
2024-01-13 19:00:41 +1000

    add swagger in main ts

--- server/src/main.ts
+++ server/src/main.ts
@@ -6,10 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Build system docs')
+    .setDescription('Build system API description')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('user')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
