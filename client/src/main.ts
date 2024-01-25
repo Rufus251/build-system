4f34fc85ffbb7c76a7d40da63fc8ccd0fa453d5f
Rufus251 <vladimerrybalkin@gmail.com>
2024-01-25 16:08:41 +1000

    added vuetify and other pockets

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,8 +2,33 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+import { mdi } from "vuetify/lib/iconsets/mdi";
+import '@mdi/font/css/materialdesignicons.css';
+
+const vuetify = createVuetify({
+      components,
+      directives,
+      icons: {
+            defaultSet: 'mdi',
+            sets: {
+                  mdi
+            }
+      },
+});
+
 const app = createApp(App)
 
-app.use(router)
+import UIComponents from "@/components/UI/index.js"
+UIComponents.forEach((component) => {
+      app.component(component.name, component)
+})
 
-app.mount('#app')
+app
+      .use(router)
+      .use(vuetify)
+      .mount('#app')
