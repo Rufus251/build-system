2c0d86f6a81d3a780e7d135d23509b8b2f6aeb08
Rufus251 <vladimerrybalkin@gmail.com>
2024-01-26 22:27:40 +1000

    added authView

--- client/src/views/AuthView.vue
+++ client/src/views/AuthView.vue
@@ -1,40 +1,21 @@
 <template>
-  <navbarComp roleProp="user" nameProp="Иван Иванов"></navbarComp>
-  <header>
-    <h1>
-      hi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
-      quod.
-    </h1>
-    <p>
-      hi! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magnam cum
-      itaque possimus ex eum consequuntur voluptas libero maiores, perferendis
-      provident sit fugiat debitis eligendi, temporibus dolorum necessitatibus
-      vero quo dicta. Commodi totam, ullam impedit dolorem, illo expedita
-      suscipit et consectetur quod repudiandae numquam quia sit quidem doloribus
-      incidunt a veniam eligendi, in corporis! Voluptatibus, possimus ipsam
-      libero natus, explicabo blanditiis fugiat repellendus harum quos at animi
-      atque cum sunt! Ut corrupti maxime earum nostrum dignissimos ipsa?
-      Explicabo labore placeat reiciendis fugiat voluptatum. Quam, nobis
-      laudantium. Nisi, magnam perferendis totam quae, exercitationem mollitia
-      earum inventore facere magni nobis error deserunt nesciunt.fff
-    </p>
-    <textField
-      labelProp="giaeirg"
-      placeholderProp="123123"
-      typeProp="email"
-      v-model="variable"
-    ></textField>
-    <autocompleteField
-      labelProp="giaeirg"
-      placeholderProp="123123"
-      :itemsProp="itemsArr"
-      v-model="variable"
-    >
-    </autocompleteField>
-    <primaryTable :keysProp="keys" v-model="table"></primaryTable>
-    <primaryButton400 href="/auth">Кнопка</primaryButton400>
-    <textareaField labelProp="agra" placeholderProp="1234"></textareaField>
-  </header>
+  <main>
+    <v-form @submit.prevent v-model="valid">
+      <textField
+        labelProp="Логин"
+        placeholderProp="Ivan_Ivanov1703"
+        v-model="login"
+        :rulesProp="loginRules"
+      ></textField>
+      <textField
+        labelProp="Пароль"
+        placeholderProp="7cWfX7bZ8iJ9KUa0"
+        v-model="password"
+        :rulesProp="passwordRules"
+      ></textField>
+      <primaryButton400 @click="send()"> Войти </primaryButton400>
+    </v-form>
+  </main>
 </template>
 
 <script>
@@ -42,18 +23,32 @@ export default {
   name: "AuthView",
   data() {
     return {
-      variable: undefined,
-      itemsArr: [
-        "California",
-        "Colorado",
-        "Florida",
-        "Georgia",
-        "Texas",
-        "Wyoming",
-      ],
-      keys: ["Бетон заливка", "Монтажный кабель"],
-      table: undefined
+      valid: false,
+
+      login: "",
+      loginRules: [(v) => v.length > 0 || "Введите логин"],
+      password: "",
+      passwordRules: [(v) => v.length > 0 || "Введите пароль"],
     };
   },
+  methods:{
+    send(){
+      if(this.valid){
+        console.log("auth user");
+      }
+    }
+  }
 };
 </script>
+
+<style scoped lang="scss">
+main {
+  width: 100%;
+  height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
