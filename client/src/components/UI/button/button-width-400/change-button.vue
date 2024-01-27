a8f871c2d9738c86b8709b531ad50281da8a881a
Rufus251 <vladimerrybalkin@gmail.com>
2024-01-27 19:26:16 +1000

    added btns variants

---
+++ client/src/components/UI/button/button-width-400/change-button.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-btn
+    flat
+    class="text-white text-none"
+    color="#ffc107"
+    min-height="60px"
+    max-height="60px"
+    min-width="400px"
+    max-width="400px"
+    :href="href"
+    block>
+    <slot></slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: "changeButton400",
+  props: {
+    href: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.v-btn {
+  margin: 10px;
+
+  font-size: 24px;
+}
+</style>
--- client/src/components/UI/index.js
+++ client/src/components/UI/index.js
@@ -2,6 +2,9 @@ import textField from "./input/text-field.vue";
 import autocompleteField from "./input/autocomplete-field.vue";
 import textareaField from "./input/textarea-field.vue";
 import primaryButton400 from "./button/button-width-400/primary-button.vue";
+import agreeButton400 from "./button/button-width-400/agree-button.vue";
+import deleteButton400 from "./button/button-width-400/delete-button.vue";
+import changeButton400 from "./button/button-width-400/change-button.vue";
 import primaryRouterButton400 from "./button/button-width-400/primary-router-button.vue";
 import primaryTable from "./table/primary-table.vue";
 import navbarComp from "./common/navbar-comp.vue";
@@ -10,8 +13,11 @@ export default [
   textField,
   autocompleteField,
   primaryButton400,
+  agreeButton400,
+  deleteButton400,
+  changeButton400,
   primaryRouterButton400,
   primaryTable,
   textareaField,
-  navbarComp
+  navbarComp,
 ];
