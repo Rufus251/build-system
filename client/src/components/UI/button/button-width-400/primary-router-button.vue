cdd77532c5d017fff2d6669002a137fa6e29f3ec
Rufus251 <vladimerrybalkin@gmail.com>
2024-01-27 19:15:20 +1000

    added routerButton

---
+++ client/src/components/UI/button/button-width-400/primary-router-button.vue
@@ -0,0 +1,32 @@
+<template>
+  <router-link :to="href">
+    <v-btn
+      flat
+      class="text-white text-none"
+      color="#3d8be4"
+      min-height="60px"
+      max-height="60px"
+      min-width="400px"
+      max-width="400px"
+      block>
+      <slot></slot>
+    </v-btn>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: "primaryRouterButton400",
+  props: {
+    href: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+.v-btn {
+  margin: 10px;
+
+  font-size: 24px;
+}
+</style>
--- client/src/components/UI/index.js
+++ client/src/components/UI/index.js
@@ -2,6 +2,7 @@ import textField from "./input/text-field.vue";
 import autocompleteField from "./input/autocomplete-field.vue";
 import textareaField from "./input/textarea-field.vue";
 import primaryButton400 from "./button/button-width-400/primary-button.vue";
+import primaryRouterButton400 from "./button/button-width-400/primary-router-button.vue";
 import primaryTable from "./table/primary-table.vue";
 import navbarComp from "./common/navbar-comp.vue";
 
@@ -9,6 +10,7 @@ export default [
   textField,
   autocompleteField,
   primaryButton400,
+  primaryRouterButton400,
   primaryTable,
   textareaField,
   navbarComp
