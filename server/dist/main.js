419e2cbd0c1e45a54c0a34a372c59d1e20ba4f96
Rufus251 <vladimerrybalkin@gmail.com>
2024-02-06 14:34:18 +1000

    update cors

--- server/dist/main.js
+++ server/dist/main.js
@@ -6,8 +6,8 @@ const app_module_1 = require("./app.module");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.enableCors({
+        origin: 'http://localhost:3001/',
         credentials: true,
-        origin: true
     });
     const port = process.env.APP_PORT || 3001;
     app.setGlobalPrefix('api');
--- server/src/main.ts
+++ server/src/main.ts
@@ -5,8 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors({
+    origin:'http://localhost:3001/',
     credentials: true,
-    origin: true
   })
   const port = process.env.APP_PORT || 3001;
   app.setGlobalPrefix('api');
