1e603d7dc5b5fd56ec816ae4e841e6342d264043
Rufus251 <vladimerrybalkin@gmail.com>
2024-02-06 13:48:52 +1000

    build

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,13 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 var cors = require('cors');
+
+const corsOptions ={
+  origin:'http://localhost:3001', 
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200,
+  'Access-Control-Allow-Origin': '*',
+  'access-control-allow-credentials': true
+}
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 3001;
   app.setGlobalPrefix('api');
-  app.use(cors({
-    'Access-Control-Allow-Origin': '*'
-  }))
+  app.use(cors(corsOptions))
   const config = new DocumentBuilder()
     .setTitle('Build system docs')
     .setDescription('Build system API description')
