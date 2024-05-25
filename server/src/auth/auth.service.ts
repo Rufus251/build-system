add5608bb7b0b4a01df4eceb2f8d17d4dfc3fefe
rufus <vladimerrybalkin@gmail.com>
2024-05-25 18:04:08 +1000

    пофиксил аутентификацию

--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -1,83 +1,88 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
-import * as bcrypt from "bcrypt";
+import * as bcrypt from 'bcrypt';
 import { LoginUserDto } from './dto/login-user.dto';
 import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly databaseService: DatabaseService, private jwtService: JwtService) { }
+  constructor(
+    private readonly databaseService: DatabaseService,
+    private jwtService: JwtService,
+  ) {}
 
-    async validateUser(dto: LoginUserDto) {
-        try {
-            let user = await this.databaseService.user.findFirst({
-                where: {
-                    login: dto.login
-                }
-            })
-            if (!user) {
-                throw new HttpException('User Not Found', HttpStatus.NOT_FOUND)
-            }
+  async validateUser(dto: LoginUserDto) {
+    try {
+      let user = await this.databaseService.user.findFirst({
+        where: {
+          login: dto.login,
+        },
+      });
+      if (!user) {
+        throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+      }
 
-            const correctPassword = bcrypt.compareSync(dto.password, user.password)
+      const correctPassword = bcrypt.compareSync(dto.password, user.password);
 
-            if (!correctPassword) {
-                throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED)
-            }
-            return user
-        } catch (error) {
-            switch (error.status) {
-                case 401:
-                    throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED)
-                case 404:
-                    throw new HttpException('User Not Found', HttpStatus.NOT_FOUND)
-                default:
-                    throw new HttpException('Error', HttpStatus.BAD_REQUEST)
-            }
-        }
+      if (!correctPassword) {
+        throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED);
+      }
+      return user;
+    } catch (error) {
+      console.log(error);
+      switch (error.status) {
+        case 401:
+          throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED);
+        case 404:
+          throw new HttpException('User Not Found', HttpStatus.NOT_FOUND);
+        default:
+          throw new HttpException('Error', HttpStatus.BAD_REQUEST);
+      }
     }
+  }
 
-    async loginPassword(dto: LoginUserDto) {
-        const valid = await this.validateUser(dto)
-        const jwt = this.jwtService.sign(dto)
-        if (valid) {
-            await this.databaseService.user.update({
-                where: {
-                    id: valid.id
-                },
-                data: {
-                    token: jwt
-                }
-            })
-        }
-        return { ...valid, jwt }
+  async loginPassword(dto: LoginUserDto) {
+    const user = await this.validateUser(dto);
+    const { login, password, role } = user;
+    const jwt = this.jwtService.sign({ login, password, role });
+    if (user) {
+      await this.databaseService.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          token: jwt,
+        },
+      });
     }
+    user.token = jwt
+    return user;
+  }
 
-    async loginJwt(token: string) {
-        try {
-            const sign = this.jwtService.verify(token)
-            if (sign) {
-                const user = await this.databaseService.user.findFirst({
-                    where: {
-                        login: sign.login
-                    }
-                })
-                const login = user.login
-                const password = user.password
-                const newToken = this.jwtService.sign({ login, password })
-                const res = await this.databaseService.user.update({
-                    where: {
-                        id: user.id
-                    },
-                    data: {
-                        token: newToken
-                    }
-                })
-                return res
-            }
-        } catch (error) {
-            console.log(error);
-            throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED)
-        }
+  async loginJwt(token: string) {
+    try {
+      const sign = this.jwtService.verify(token);
+      if (sign) {
+        const user = await this.databaseService.user.findFirst({
+          where: {
+            login: sign.login,
+          },
+        });
+        const { login, password, role } = user;
+        const newToken = this.jwtService.sign({ login, password, role });
+        const res = await this.databaseService.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            token: newToken,
+          },
+        });
+        return res;
+      }
+    } catch (error) {
+      console.log(error);
+      throw new HttpException('Unauthorised', HttpStatus.UNAUTHORIZED);
     }
+  }
 }
